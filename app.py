df7c0b1280d0a1a6a836993d4edc27371ea30c89
paarvi28 <138505737+paarvi28@users.noreply.github.com>
2026-05-04 15:14:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ import pandas as pd
 
 st.title("🎬 BingeWatch Recommendation System")
 
-st.write("Current files in directory:")
-st.write(os.listdir())
-
+# Load dataset
 try:
-    df = pd.read_csv("MoviesOnStreamingPlatforms.csv")
+    df = pd.read_csv("final_movies.csv")
     st.success("Dataset loaded successfully!")
 except Exception as e:
     st.error(f"Error loading dataset: {e}")
