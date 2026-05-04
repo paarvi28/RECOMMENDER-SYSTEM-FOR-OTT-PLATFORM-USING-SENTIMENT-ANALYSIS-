5ff0adf95aeabe86eafd0921e06e22f482f409d0
paarvi28 <138505737+paarvi28@users.noreply.github.com>
2026-05-04 15:08:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
+import os
 import streamlit as st
 import pandas as pd
 
 st.title("🎬 BingeWatch Recommendation System")
 
+st.write("Current files in directory:")
+st.write(os.listdir())
+
 try:
     df = pd.read_csv("MoviesOnStreamingPlatforms.csv")
-except:
-    st.error("Dataset not found. Please check path.")
-    st.stop()
-
-genre = st.selectbox("Select Genre", df['listed_in'].dropna().unique())
-
-filtered = df[df['listed_in'].str.contains(genre, na=False)]
-
-st.subheader("Recommended Content:")
-st.write(filtered[['title', 'description']].head(10))
-
-try:
-    
     st.success("Dataset loaded successfully!")
 except Exception as e:
     st.error(f"Error loading dataset: {e}")
